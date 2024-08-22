b08d7616f51d7d1a50b2fed16970f2f85133c5bf
Hunter100102 <spc.cody.hunter@gmail.com>
2024-08-21 23:43:40 -0400

    This is just a temp file

---
+++ practice.py
@@ -0,0 +1,24 @@
+# Set up a letter counter in a string. Count the top 3 most occuring letters and then print the letter and the number of occurances of that letter. Next if the number
+# of occurances are the same put them in alphabetical order. 
+def counter(name):
+    letters = {}
+    name = name.lower()
+    for letter in name:
+        if letter in letters:
+            letters[letter] += 1
+        else:
+            letters[letter] = 1
+        
+    #letters = letters(sorted(name.items, key=lambda item: item[1], reverse=True))
+    top = list(letters.items())[:3]
+
+    for key, value in top:
+        print(f"{key} {value}")
+
+
+
+
+
+if __name__ == '__main__':
+    value = "Gogloe"
+    counter(value)
