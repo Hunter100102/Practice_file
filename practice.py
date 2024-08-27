2f96830f2599a727f627de4432834a6c885eec2c
Hunter100102 <spc.cody.hunter@gmail.com>
2024-08-26 21:26:11 -0400

    Next Go

--- practice.py
+++ practice.py
@@ -8,13 +8,24 @@
 
 
 #You are given a string and you need to find the first occurance of the repeating alphanumeric character, and if there is not one the print -1.
-import re
+#import re
 
-s = "__commit__"
+#s = "__commit__"
 
-match = re.search(r'([a-zA-Z0-9])\1', s)
+#match = re.search(r'([a-zA-Z0-9])\1', s)
 
-if match:
-    print(match.group(1))
-else:
-    print(-1)
+#if match:
+    #print(match.group(1))
+#else:
+    #print(-1)
+
+#Find all substrings that contain two or more vowels.
+s = "rabcdeefgyYhFjkIoomnpOeorteeeeet"
+
+
+
+#Output
+#ee
+#Ioo
+#Oeo
+#eeeee
