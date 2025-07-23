38e8142ce9589dce64cb414e1a17b2fddc9739aa
Hunter100102 <136289387+Hunter100102@users.noreply.github.com>
2025-07-23 12:08:26 -0400

    Update practice.py

--- practice.py
+++ practice.py
@@ -584,11 +584,15 @@
 # >>> Single-line Comment
 # [if IE 9]>IE9-specific content<![endif]
 
+#Start Code here
 from html.parser import HTMLParser
 
 class MyHTMLParser(HTMLParser):
     def handle_comment(self, data):
         print("Comment: ", data)
+    
+    def handle_data(self, data):
+        print("Data :", data)
   
   
 i = 4
