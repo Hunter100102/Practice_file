2f832dc5d489ed0fc15a94109f0ad143af330759
Hunter100102 <spc.cody.hunter@gmail.com>
2025-09-25 15:17:34 -0400

    Next practice.

--- practice.py
+++ practice.py
@@ -740,3 +740,45 @@
 #     arr = [2,3,6,6,5]
 #     runner_up(n, arr)
 #-----------------------------------------------------------------------------------------------------------------------------------------
+import math
+
+class Complex(object):
+    def __init__(self, real, imaginary):
+        
+    def __add__(self, no):
+        
+    def __sub__(self, no):
+        
+    def __mul__(self, no):
+
+    def __truediv__(self, no):
+
+    def mod(self):
+
+    def __str__(self):
+        if self.imaginary == 0:
+            result = "%.2f+0.00i" % (self.real)
+        elif self.real == 0:
+            if self.imaginary >= 0:
+                result = "0.00+%.2fi" % (self.imaginary)
+            else:
+                result = "0.00-%.2fi" % (abs(self.imaginary))
+        elif self.imaginary > 0:
+            result = "%.2f+%.2fi" % (self.real, self.imaginary)
+        else:
+            result = "%.2f-%.2fi" % (self.real, abs(self.imaginary))
+        return result
+
+
+if __name__ == '__main__':
+    # Hard-coded sample input
+    C = [2.0, 1.0]
+    D = [5.0, 6.0]
+    x = Complex(*C)
+    y = Complex(*D)
+    print(x + y)
+    print(x - y)
+    print(x * y)
+    print(x / y)
+    print(x.mod())
+    print(y.mod())
